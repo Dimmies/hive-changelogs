b225c92be356c69cca42e3cc03e284c4f7dbdbe0
Dimmies <dimmiestv@gmail.com>
2021-08-01 12:14:06 -0400

    Add Keybinds List

--- keybinds.js
+++ keybinds.js
@@ -1,3 +1,6 @@
+$(".tt-warn").addClass("fas fa-exclamation-circle fa-xs")
+$(".tt-warn").addClass("fa-exclamation-circle fa-xs")
+$(".tt-warn").addClass("fa-xs")
 $(document).ready(function() {
 	setTimeout(() => {
 		Tipped.create('.tt-warn', "Configurable in Keybind Settings!", { position: 'right', size: 'x-small' });
